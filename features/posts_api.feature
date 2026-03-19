6ecd05442e02285b02eb559470acf27ca7294675
Valéria Santos <valeriasantos.b@gmail.com>
2026-03-19 10:13:11 -0300

    feat: cenario de post

--- features/posts_api.feature
+++ features/posts_api.feature
@@ -1,14 +1,12 @@
 Feature: Posts API
   Validate operations for posts endpoints.
 
-
-Scenario: Retrieve all posts successfully
+  Scenario: Retrieve all posts successfully
     When I send a GET request to "/posts"
     Then the response status should be 200
     And the response should contain a list of posts
 
-
-Scenario: Retrieve a specific post by ID successfully
+  Scenario: Retrieve a specific post by ID successfully
     When I send a GET request to "/posts/1" with a valid id
     Then the response status should be 200
     And the response should contain a post with id 1
@@ -21,3 +19,19 @@ Scenario: Retrieve a specific post by ID successfully
       nostrum rerum est autem sunt rem eveniet architecto
       """
 
+  Scenario Outline: Create a new post with different valid data
+    When I send a POST request to "/posts" with the following body:
+      """
+      {
+        "title": "<title>",
+        "body": "<body>",
+        "userId": <userId>
+      }
+      """
+    Then the response status should be 201
+    And the response should contain a post with title "<title>"
+    And the response should contain a post with body "<body>"
+
+    Examples:
+      | title                  | body                                                    | userId |
+      | 101 Dalmatians Request | Request body for creating a new post for 101 Dalmatians | 101    |
--- features/step_definitions/posts_api.rb
+++ features/step_definitions/posts_api.rb
@@ -1,61 +1,60 @@
 require 'httparty'
 require 'rspec'
 
-When('I send a GET request to {string}') do |endpoint|
-  
-    @response = HTTParty.get("#{BASE_URL}/#{endpoint}")
-    log("<details><summary>View response body</summary><b><br>Returned Status Code: #{@response.code}</b><pre>#{@response.body}</pre></details>")
+def validate_post_body(expected_post_body)
+  parsed = JSON.parse(@response.body)
+  expect(parsed["body"]).to eq(expected_post_body)
+end
 
+When('I send a GET request to {string}') do |endpoint|
+  @response = HTTParty.get("#{BASE_URL}#{endpoint}")
+  log("<details><summary>View response body</summary><b><br>Returned Status Code: #{@response.code}</b><pre>#{@response.body}</pre></details>")
 end
 
 Then('the response status should be {int}') do |status_code|
-    expect(@response.code).to eq(status_code)
-
-
+  expect(@response.code).to eq(status_code)
 end
 
 Then('the response should contain a list of posts') do
-    body = JSON.parse(@response.body)
-    expect(body).to be_an(Array)
+  body = JSON.parse(@response.body)
+  expect(body).to be_an(Array)
 end
-
-
 
 Then('the response should contain a post with id {int}') do |expected_id|
-  
-    
-
+  parsed = JSON.parse(@response.body)
+  expect(parsed["id"]).to eq(expected_id)
 end
-
 
 When('I send a GET request to {string} with a valid id') do |endpoint|
   @id = 1
-  @response = HTTParty.get("#{BASE_URL}/posts/#{@id}")
+  @url = "#{BASE_URL}/posts/#{@id}"
+  @response = HTTParty.get(@url)
+
   log("<details><summary>View response body</summary><b><br>Returned Status Code: #{@response.code}</b><pre>#{@response.body}</pre></details>")
-
-end
-
-Then('the response should contain a post with id') do
-    parsed = JSON.parse(@response.body)
-
-    expect(parsed["id"]).to eq(@id)
-end
-
-Then('the response should contain a post with {int}') do |expected_id|
-    parsed = JSON.parse(@response.body)
-
-    expect(parsed["id"]).to eq(expected_id)
 end
 
 Then('the response should contain a post with title {string}') do |expected_field_title|
-    parsed = JSON.parse(@response.body)
-
-    expect(parsed["title"]).to eq(expected_field_title)
+  parsed = JSON.parse(@response.body)
+  expect(parsed["title"]).to eq(expected_field_title)
 end
 
+# Step para string literal
+Then('the response should contain a post with body {string}') do |expected_post_body|
+  validate_post_body(expected_post_body)
+end
 
+# Step para doc string
 Then('the response should contain a post with body:') do |expected_post_body|
-    parsed = JSON.parse(@response.body)
+  validate_post_body(expected_post_body)
+end
 
-    expect(parsed["body"]).to eq(expected_post_body)
+When('I send a POST request to {string} with the following body:') do |endpoint, request_body|
+  @url = "#{BASE_URL}/#{endpoint}"
+  headers = { 'Content-Type' => 'application/json' }
+
+  @response = HTTParty.post(@url,
+    headers: headers,
+    body: request_body
+  )
+
 end
